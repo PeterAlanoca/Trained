c4181a8f987f8b40b0e241b49b80435edc37e8da
Peter Alanoca <peter.alanoca@gmail.com>
2022-08-20 16:57:15 -0400

    add login and dependency

--- app/src/main/java/com/boliviandeveloper/netflix/view/ui/login/LoginActivity.kt
+++ app/src/main/java/com/boliviandeveloper/netflix/view/ui/login/LoginActivity.kt
@@ -2,11 +2,37 @@ package com.boliviandeveloper.netflix.view.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.boliviandeveloper.netflix.R
+import androidx.core.widget.doOnTextChanged
+import com.boliviandeveloper.netflix.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.emailEditText.doOnTextChanged { _, _, _, _ ->
+            validateLoginButton()
+        }
+        binding.passwordEditText.doOnTextChanged { _, _, _, _ ->
+            validateLoginButton()
+        }
+        binding.loginButton.setOnClickListener {
+            println("LOGIN")
+        }
+        binding.backImageView.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun validateLoginButton() {
+        binding.emailEditText.text?.let { email ->
+            binding.passwordEditText.text?.let { password ->
+                binding.loginButton.isEnabled = (email.trim().isNotEmpty() && password.trim().isNotEmpty())
+            }
+        }
     }
 }
